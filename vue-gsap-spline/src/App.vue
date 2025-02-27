60d7d6c784965396d55c6b7bf442491cf3c2b656
KurtSchwimmbacher <231002@virtualwindow.co.za>
2025-02-27 20:27:59 +0200

    add spline component

---
+++ vue-gsap-spline/src/App.vue
@@ -0,0 +1,32 @@
+<script setup>
+import { ref, onMounted } from 'vue';
+import { gsap } from 'gsap';
+import '@splinetool/viewer';
+
+const box = ref(null);
+
+</script>
+
+<template>
+   <div class="hero">
+    <spline-viewer url="https://prod.spline.design/TUufPLGVEZGWCiED/scene.splinecode"></spline-viewer>
+  </div>
+</template>
+
+<style>
+body {
+  background-color: #A8B2B5; 
+  color: #1f1f1f; 
+}
+
+.hero{
+  border: 2px solid#1f1f1f;
+  width: 100vw;
+  height: 100vh;
+  margin-left: -11% !important;
+  margin-top: -3%;
+  overflow: hidden !important;
+}
+
+
+</style>
